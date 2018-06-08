57b49cddcfcd9ded18a7eae0b58fb0c23762e43a
Rhi88 <rhizdo_nop@hotmail.com>
2018-06-08 11:21:37 -0500

    Clase cuadrado modificada por los comentarios

--- IsraelOrozcoGuerrero/Clase_cuadrado.js
+++ IsraelOrozcoGuerrero/Clase_cuadrado.js
@@ -1,28 +1,30 @@
 var console=require ("console")
 
-class Forma{
+class Forma
+{
 
 	constructor(id,x,y)
+		{
+		this.id=id
+		this.mover(x,y)
+		}
+	mover(x,y)
+		{
+		this.x=x
+		this.y=y
+		}
+}
+class Cuadrado extends Forma
 {
-this.id=id
-this.mover(x,y)
-}
-mover(x,y)
-{
-this.x=x
-this.y=y
-}
-}
-class Cuadrado extends Forma{
 	constructor(id,x,y,lado)
-{
+	{
 	super(id,x,y)
 	this.lado=lado
 
+	}
 }
-´}
 var cuadrado= new Cuadrado(1,5,7,10);
 console.log(cuadrado.id);
 console.log(cuadrado.x);
 console.log(cuadrado.y);
-console.log(cuadrado.lado):	
+console.log(cuadrado.lado);
